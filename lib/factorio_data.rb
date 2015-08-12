4e38aa2a0cae33893e3649767a1fadde6a89fb23
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-08-12 12:06:52 +0100

    Cute attempt at manually implementing require, but this is not really workable

--- lib/factorio_data.rb
+++ lib/factorio_data.rb
@@ -1,10 +1,60 @@
 require "ruby-lua"
 require "pathname"
 
+# This is dumb...
+def lua_trampoline(*args)
+  $__lua_env__.send(*args)
+end
+
+# Perl style local vars, oh yeah!
+def with_lua_trampoline(obj)
+  prev = $__lua_env__
+  $__lua_env__ = obj
+  yield
+ensure
+  $__lua_env__ = prev
+end
+
+
 class FactorioData
   def initialize(path)
-    @lua = Language::Lua.new()
     @path = Pathname(path)
+    setup_lua!
   end
 
+  def load!
+    load_lua!(@path + "data/base/data.lua")
+  end
+
+  def load_lua!(path)
+    with_lua_trampoline(self) do
+      @lua.eval(path.read)
+    end
+  end
+
+  def lua_require(path)
+    load_lua! resolve_require_path(path)
+    require 'pry'; binding.pry
+  end
+
+  def resolve_require_path(req)
+    search_path = %W[factorio/data/core/lualib]
+    subpath = req.split(".").join("/") + ".lua"
+    search_path.each do |dir|
+      candidate = Pathname(dir) + subpath
+      return candidate if candidate.exist?
+    end
+    raise "Can't find #{req} in lua search path"
+  end
+
+  private
+
+  def setup_lua!
+    @lua = Language::Lua.new()
+    @lua.eval <<-LUA
+      function require(path)
+        ruby("lua_trampoline", "lua_require", path)
+      end
+    LUA
+  end
 end
