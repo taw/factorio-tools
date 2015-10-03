cbbd5a648f68ba2f19e92702a811131b59558c6e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-03 15:09:35 +0100

    The whole Better Visuals minimod

---
+++ mods/Better Visuals_0.0.1/data.lua
@@ -0,0 +1,10 @@
+-- works on new or existing campaigns
+data.raw["player"]["player"].light = {
+  {
+    minimum_darkness = 0.3,
+    intensity = 1.0,
+    size = 150,
+  },
+};
+-- only works on new campaign
+data.raw["map-settings"]["map-settings"].pollution.expected_max_per_chunk = 10000000000;
