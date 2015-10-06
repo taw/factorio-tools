38a03ae9f8a84408a8d7f833a3ff528db958abf8
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-06 02:06:32 +0100

    Nice halo for car too

--- mods/Better Visuals_0.0.1/data.lua
+++ mods/Better Visuals_0.0.1/data.lua
@@ -6,5 +6,12 @@ data.raw["player"]["player"].light = {
     size = 150,
   },
 };
+data.raw["car"]["car"].light = {
+  {
+    minimum_darkness = 0.3,
+    intensity = 1.0,
+    size = 250,
+  },
+};
 -- only works on new campaign
 data.raw["map-settings"]["map-settings"].pollution.expected_max_per_chunk = 10000000000;
